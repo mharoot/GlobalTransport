8c67d9a89a15c5cf40647c896eb3002daf7ed963
mharoot <michaelharootoonyan@yahoo.com>
2018-08-12 00:06:38 -0700

    broke up tests perfectly, works good

--- global-software2/protected/tests/unit/GlobalTrackerOrderTest.php
+++ global-software2/protected/tests/unit/GlobalTrackerOrderTest.php
@@ -12,7 +12,40 @@ class GlobalTrackerOrderTest extends TestCase
 
         Yii::createWebApplication($config);
         Yii::app()->getModule('GlobalTrackerOrder');
+        Yii::app()->getModule('UserIdentity');
+        
+
         $this->assertTrue(true);
+    }
+
+    /**
+     * Used by:
+     * @method testSearch()
+     */
+    public function testLogin()
+    {
+        $identity = new UserIdentity("tonygreg", "password");
+        
+        $this->assertTrue( $identity->authenticate() );
+        $duration_days = 0;
+        $this->assertTrue( Yii::app()->user->login($identity, $duration_days) );
+    }
+
+    /**
+     * uses @method testLogin()
+     */
+    public function testSearch()
+    {
+        $status = "1";
+        $perPage = 50;
+
+        $order58 = GlobalTrackerOrder::model()->findByPk(58);
+        $this->assertEquals($order58->getAttributes()['status'], $status);
+        
+        
+        $res = $order58->search($status, $perPage);
+        var_dump($res->model->address1);
+        $this->assertEquals("2009 Burbank Blvd", $res->model->address1);
     }
 
     public function testTableName()
@@ -20,7 +53,8 @@ class GlobalTrackerOrderTest extends TestCase
         
         $OrderModel = new GlobalTrackerOrder;
         $name = $OrderModel->tableName();
-        printf("\n\nTablename = %s\n", $name);
-        $this->assertTrue( isset($name) );
+        //printf("\n\nTablename = %s\n", $name);
+        $this->assertEquals("global_tracker_order", $name);
     }
+    
 }
